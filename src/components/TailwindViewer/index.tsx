f9ac850fa53aa0e97558d9f1fcfe9c0c93ca60e2
wesleyara <wes1gom@gmail.com>
2024-07-24 12:55:34 -0300

    feat: Add TailwindViewer component and improve scripts

--- src/components/TailwindViewer/index.tsx
+++ src/components/TailwindViewer/index.tsx
@@ -4,31 +4,37 @@ import { FaExpand } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import "../../styles/globals.css";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const TailwindViewer = () => {
   const [isExpandedOpen, setIsExpandedOpen] = useState(false);
 
   return (
-    <Popover className="relative">
-      <PopoverButton className="btn">
-        <RiTailwindCssFill color="#38BDF8" size={24} />
-      </PopoverButton>
-      <PopoverPanel
-        anchor="bottom"
-        className={`container ${isExpandedOpen ? "expanded" : ""}`}
-      >
-        <span className="btn-box">
-          <button
-            className="btn-actions"
-            onClick={() => setIsExpandedOpen(prev => !prev)}
+    <>
+      {!isProd && (
+        <Popover className="relative">
+          <PopoverButton className="btn">
+            <RiTailwindCssFill color="#38BDF8" size={24} />
+          </PopoverButton>
+          <PopoverPanel
+            anchor="bottom"
+            className={`container ${isExpandedOpen ? "expanded" : ""}`}
           >
-            <FaExpand size={18} />
-          </button>
-        </span>
-        <iframe
-          src="https://rogden.github.io/tailwind-config-viewer/"
-          className="frame"
-        ></iframe>
-      </PopoverPanel>
-    </Popover>
+            <span className="btn-box">
+              <button
+                className="btn-actions"
+                onClick={() => setIsExpandedOpen(prev => !prev)}
+              >
+                <FaExpand size={18} />
+              </button>
+            </span>
+            <iframe
+              src="https://rogden.github.io/tailwind-config-viewer/"
+              className="frame"
+            ></iframe>
+          </PopoverPanel>
+        </Popover>
+      )}
+    </>
   );
 };
